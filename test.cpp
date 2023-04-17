5d88b56443f0afc51a1bb3b331fd74b96f958b27
Avging <2178881296@qq.com>
2023-04-17 22:21:11 +0800

    Test

--- test.cpp
+++ test.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Han on 2023/4/17.
 //
+#include <iostream>
+using namespace std;
+
+class C {
+public:
+    void test() {
+        cout << "test" ;
+    }
+};
+
+int main() {
+    C c;
+    c.test();
+
+    C* ci = new C();
+    ci->test();
+}
